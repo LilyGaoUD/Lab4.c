c185d2e53be489e42d2d7895f3aa0016041fc38c
LilyGaoUD <127171294+LilyGaoUD@users.noreply.github.com>
2023-03-06 22:39:10 -0500

    Create Lab4.c

---
+++ Lab4.c
@@ -0,0 +1,10 @@
+#include<stdlib.h>
+int main (int argc,char**argv){
+        int sum = 0;
+        if(argc>5)
+                return 0;
+        else
+                for(int x=1; x<argc;x++){
+                        sum = sum+atoi(argv[s]);
+                }
+        return sum;}
